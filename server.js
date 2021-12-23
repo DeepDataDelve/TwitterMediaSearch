630dbccbbac2708c9bb8da273605be49dcce83a9
Isaac Schneider <50723138+DeepDataDelve@users.noreply.github.com>
2021-12-23 12:06:45 -0500

    Add files via upload
    updated for new packages/updates

--- server.js
+++ server.js
@@ -1,27 +1,8 @@
 var express = require('express');
 var app = express();
-var log4js = require("log4js");
-var morgan = require("morgan");
-var fs = require("fs");
-log4js.configure({ // configure to use all types in different files.
-  appenders: {
-    out: { type: 'stdout' },
-    app: { type: 'file', filename: 'logs/application.log' }
-  },
-  categories: {
-    default: { appenders: [ 'out', 'app' ], level: 'debug' }
-  }
-});
-
-var accessLogStream = fs.createWriteStream(('logs/access.log'), { flags: 'a' });
-var HTTPLog = morgan(
-  'combined',
-  {
-    "stream": accessLogStream//{ write: function(str) { theAppLog.all(str); } }
-  }
-);
-app.use(HTTPLog);
+var morgan = require('morgan');
 const rateLimit = require("express-rate-limit");
+ 
 app.enable("trust proxy"); // only if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
  
 const limiter = rateLimit({
@@ -31,8 +12,9 @@ const limiter = rateLimit({
 
 var UserController = require('./controllers/TwitStatsController');
 app.use('/TwitReactionLog', UserController);
+
 //  apply to all requests
-//app.use(morgan('combined')); //logger for api, standard apache format
+app.use(morgan('combined')); //logger for api, standard apache format
 app.use(limiter);
 
 let port = 8210;
